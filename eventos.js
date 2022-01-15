fcb185a15c20389011886a98d3cf1adad01cb5bb
Luciano-do-Carmo <lucianodocarmo111@gmail.com>
2022-01-15 11:13:35 -0300

    adicionando arquivo eventos

---
+++ eventos.js
@@ -0,0 +1,26 @@
+//sistema de cadastro de eventos-primeiro requisito idade 
+let idade = 20
+if(idade>=18) {
+    console.log("pode realizar o cadastro, pois a idade esta de acordo")
+} else {
+    console.log("idade nao permitida para realizar o cadastro")
+}
+//segundo requisito quantidade de participantes
+let quantidadedeparticipantes = 100
+if(quantidadedeparticipantes<=100) {
+    console.log("pode fazer o cadastro")
+} else {
+    console.log("não e permitido cadastrar, devido ultrapassar o limite de participantes")
+}
+// terceiro requisito data do evento
+let dataevento = new Date("January 21, 2022 12:00:00");
+let dataatual = new Date("January 15, 2022 15:00:00");
+
+if (dataatual < dataevento) {
+  console.log("pode cadastrar evento, pois a data esta de acordo");
+} else { 
+  console.log("nao pode marcar o evento");
+}
+// quarto requisito condição    
+const participantes= ["Luciano, Arthur, Vitor, Joao, Maria, Pedro"]
+console.log (participantes)
